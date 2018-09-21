ee4660e315fbfce977b16dff1cc7c55d5b1924a8
WaysideTester <dylanmurray2119@gmail.com>
2018-09-20 20:51:44 -0500

    WIP

--- main.js
+++ main.js
@@ -1,27 +1,42 @@
+// fucntions
 const printToDom = (divId, stringToPrint) => {
     document.getElementById(divId).innerHTML += stringToPrint;
 };
 
+
+// Data
 const separateButEqual = ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin'];
-
-printToDom('sorted-pupils', "test 1");
-printToDom('sorted-pupils', "test 2");
-printToDom('sorted-pupils', "test b");
-
+let fName = document.getElementById('first-name');
+let lName = document.getElementById('last-name');
+let fValue = fName.value;
+let lValue = lName.value;
+fName.setAttribute("autocomplete", "off");
+lName.setAttribute("autocomplete", "off");
+// selectors
 const submitName = document.getElementById('sub-Name');
-
-
-if (submitName.addEventListener){
-    submitName.addEventListener("click", (e) => {
-        e.preventDefault();
-        console.log('something');
-    })
-}
-
-const numGenerator = () => {
-    return Math.round(Math.random()*3);
-};
-
 const houseSelector = () => {
     return separateButEqual[numGenerator()];
 };
+const numGenerator = () => {
+    return Math.round(Math.random()*(separateButEqual.length-1));
+};
+
+submitName.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    fValue = fName.value;
+    lValue = lName.value;
+    let newString = ``;
+    newString += `  <div class="card w-50">
+            <div class="card-body">
+                <h5 class="card-title">${fValue} ${lValue}</h5>
+                <p class="card-text">${houseSelector()}</p>
+                <a href="#" class="btn btn-primary">Expel</a>
+            </div>
+        </div>`;
+    if (fValue && lValue !== ''){
+        printToDom('sorted-pupils',newString);
+        fValue.value = null;
+        lValue.value = null;
+    }
+})
