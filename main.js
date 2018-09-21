7fb5a8ab92110ed23925c3b1eb00cadc685e9bc7
WaysideTester <dylanmurray2119@gmail.com>
2018-09-20 19:45:34 -0500

    that was easy

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const printToDom = (divId, stringToPrint) => {
     document.getElementById(divId).innerHTML += stringToPrint;
 };
 
+const separateButEqual = ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin'];
+
 printToDom('sorted-pupils', "test 1");
 printToDom('sorted-pupils', "test 2");
 printToDom('sorted-pupils', "test b");
