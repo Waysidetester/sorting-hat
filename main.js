aef21671a80d403c92bc1dffade8753d280703eb
WaysideTester <dylanmurray2119@gmail.com>
2018-09-20 21:18:09 -0500

    expel works and submission refocuses input box

--- main.js
+++ main.js
@@ -14,16 +14,15 @@ fName.setAttribute("autocomplete", "off");
 lName.setAttribute("autocomplete", "off");
 // selectors
 const submitName = document.getElementById('sub-Name');
-const houseSelector = () => {
+const houseSelector = () => { //uses randomly generated number to input house name
     return separateButEqual[numGenerator()];
 };
-const numGenerator = () => {
+const numGenerator = () => { //Generates random number
     return Math.round(Math.random()*(separateButEqual.length-1));
 };
 
 submitName.addEventListener("click", (e) => {
     e.preventDefault();
-
     fValue = fName.value;
     lValue = lName.value;
     let newString = ``;
@@ -31,12 +30,22 @@ submitName.addEventListener("click", (e) => {
             <div class="card-body">
                 <h5 class="card-title">${fValue} ${lValue}</h5>
                 <p class="card-text">${houseSelector()}</p>
-                <a href="#" class="btn btn-primary">Expel</a>
+                <a href="#" class="btn btn-primary expel-this">Expel</a>
             </div>
         </div>`;
+    // Prevents card generation if a field is left blank
     if (fValue && lValue !== ''){
         printToDom('sorted-pupils',newString);
         fName.value = null;
         lName.value = null;
+        fName.focus();
+    }
+    const expel = document.getElementsByClassName('expel-this');
+    // adds remove button for each card
+    for (i = 0; i < expel.length; i++){
+        expel[i].addEventListener('click', (e) => {
+            e.preventDefault(); //prevents page from auto scrolling
+            e.target.parentNode.parentNode.remove();
+    })
     }
 })
